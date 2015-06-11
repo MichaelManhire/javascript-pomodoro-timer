42fe2a3b79f98fbf3bd16b0c0a6d8e1fca3f1354
Michael Manhire <MichaelManhire@gmail.com>
2015-06-11 14:27:43 -0700

    Add reset functions
    When called, change the value of the variables based on whether a new
    pomodoro, a short break, or a long break is next

--- js/app.js
+++ js/app.js
@@ -9,3 +9,28 @@ var mins = 25,
     currentSecs = 0,
     currentMins = 0,
     comingOffBreak = false;
+
+function resetCountdown() {
+    // For new pomodoro
+    mins = 25;
+    secs = mins * 60;
+    currentSecs = 0;
+    currentMins = 0;
+    comingOffBreak = false;
+};
+
+function resetForShortBreak() {
+    mins = 5;
+    secs = mins * 60;
+    currentSecs = 0;
+    currentMins = 0;
+    comingOffBreak = true;
+}
+
+function resetForLongBreak() {
+    mins = 30;
+    secs = mins * 60;
+    currentSecs = 0;
+    currentMins = 0;
+    comingOffBreak = true;
+}
